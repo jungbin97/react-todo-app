f5f21d85172147185a7a8babc196cfa5048399fb
jungbin97 <ljb5054@gmail.com>
2023-09-13 16:13:36 +0900

    feat: tailwindcss css 설정

--- src/components/List.js
+++ src/components/List.js
@@ -2,24 +2,6 @@ import React from 'react'
 
 // Props로 필요한 데이터 함수 가져오기
 export default function List({todoData, setTodoData}) {
-    const btnStyle = {
-        color: "#fff",
-        border: "none",
-        padding: "5px 9px",
-        borderRadius: "50%",
-        cursor: "pointer",
-        float: "right",
-    };
-    
-    const getStyle = (completed) => {
-    return {
-        padding: "10px",
-        borderBottom: "1px #ccc dotted",
-        textDecoration: completed ? "line-through": "none",
-
-        };
-    };
-
     const handleClick = (id) =>{
         let newTodoData = todoData.filter(data => data.id !== id);
         setTodoData(newTodoData);
@@ -39,12 +21,19 @@ export default function List({todoData, setTodoData}) {
     return (
     <div>
         {todoData.map((data) => (
-            <div style={getStyle(data.completed)} key={data.id}>
-                <input type="checkbox" defaultChecked={false} onChange={() => handleCompleChange(data.id)} />
-                    {data.title}
-                <button style={btnStyle} onClick={() => handleClick(data.id)}>X</button>
+            <div key={data.id}>
+                <div className='flex items-center justify-between w-full px-4
+                py-1 my-2 text-gray-600 bg-gray-100 border rounded'>
+                    <div className='items-center'>
+                        <input type="checkbox" defaultChecked={false} onChange={() => handleCompleChange(data.id)} />
+                            <span className={data.completed ? undefined:'line-through'}> {data.title}</span>
+                    </div>
+                    <div className='items-center'>
+                        <button className='px-4 py-2 float-right' onClick={() => handleClick(data.id)}>X</button>
+                    </div>
             </div>
+        </div>
         ))}
     </div>
-    )
+    );
 }
